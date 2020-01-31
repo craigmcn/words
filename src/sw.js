d263d5d8a63c5cf74cac46f98544720bebcb43fe
Craig McNaughton <therealcraigmcn@gmail.com>
2020-01-30 19:06:08 -0800

    Add PWA querystring
    + update cache version

--- src/sw.js
+++ src/sw.js
@@ -14,13 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-57'
+const PRECACHE = 'precache-58'
 const RUNTIME = 'runtime'
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
+  './',
+  './?source=pwa',
   'index.html',
-  './', // Alias for index.html
   './css/albert.min.css',
   './css/styles.css',
   './js/scripts.js',
